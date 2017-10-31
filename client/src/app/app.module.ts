c6427024229904d892bfbdab1453312f6e99dcdf
FLAVIOALFA <dueplotters@gmail.com>
2017-10-31 20:01:31 -0300

    Add files via upload

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,27 +5,33 @@ import { NgModule } from '@angular/core';
 import { APP_ROUTING } from './app.routes';
 //******************* Fin Rutas *********************
 
-// ********************* Servicios *********************
+import { CalendarModule } from 'angular-calendar';
 
+// ********************* Servicios *********************
+import { AlojamientoService } from "./services/alojamiento.service";
 // ******************* Fin Servicios *******************
 // *********************** Componentes ***********************
 import { AppComponent } from './app.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { AlojamientoComponent } from './components/alojamiento/alojamiento.component';
 import { HomeComponent } from './components/home/home.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 // ********************* Fin Componentes *********************
 @NgModule({
   declarations: [
     AppComponent,
     UsuarioComponent,
     AlojamientoComponent,
-    HomeComponent
+    HomeComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    AlojamientoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
